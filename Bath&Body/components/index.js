6e7641595d70af2c49b7c357b7dae97c7ff84371
Ravi Kumar <99811466+rao8969@users.noreply.github.com>
2022-06-17 12:58:46 +0530

    Add files via upload

---
+++ Bath&Body/components/index.js
@@ -0,0 +1,25 @@
+function  navbar() {
+            
+            return`<div id="navbar">
+            <img id="color" src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.216/on/demandware.static/Sites-BathAndBodyWorks-Site/-/default/dwa9385fe9/images/svg-icons/Logos-main.svg?yocs=o_s_" alt="">
+            <input type="text" id="search" placeholder="Search by fragrance or product..."> 
+            <i class="fa-solid fa-user" id="profile"></i>
+            <span><i class="fa-solid fa-cart-plus" id="profile"></i></span>
+
+            </div>
+            <div id="nav">
+            <dvi id="red">ALL SALE</dvi>
+            <dvi>BODY CARE</dvi>
+            <dvi>CANDLES</dvi>
+            <dvi>HOME FRAGRANCE</dvi>
+            <dvi>HAND SHOP & SANATIZER</dvi>
+            <dvi>MEN'S</dvi>
+            <dvi>GIFTS</dvi>
+            <dvi>BEST SELLER</dvi>
+            </div>
+            </div>
+            <div>`           
+            
+}
+
+export default navbar; 
